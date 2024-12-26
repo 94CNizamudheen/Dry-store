af411e90f5d3fab96574a5712fdd444931f7145a
Nizamudheen <nisunisam2@gmail.com>
2024-12-26 14:26:39 +0530

    reward point for the google signup customer

--- config/passport.js
+++ config/passport.js
@@ -21,6 +21,7 @@ passport.use(new GoogleStrategy({
                 name:profile.displayName,
                 email:profile.emails[0].value,
                 googleId:profile.id,
+                rewardPoints:250,
             })
             await user.save();
             return done(null,user)
